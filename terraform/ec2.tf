670019ce2094f69d4a55d480201dd1d727dab6f6
kolanavya104 <navyakola104@gmail.com>
2024-09-18 11:20:50 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,12 @@
+data "aws_instance" "existing" {
+  instance_id = "i-0139c49d1891d86fe"
+}
+
+output "instance_details" {
+  value = {
+    instance_id         = data.aws_instance.existing.id
+    public_ip           = data.aws_instance.existing.public_ip
+    private_ip          = data.aws_instance.existing.private_ip
+    availability_zone   = data.aws_instance.existing.availability_zone
+  }
+}
